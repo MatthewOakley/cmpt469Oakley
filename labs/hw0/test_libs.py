7fc9ee9c3f75cafd71931161d845f067a0940ebe
matt oakley <matt oakley>
2017-09-11 11:48:02 -0400

    first commit

---
+++ labs/hw0/test_libs.py
@@ -0,0 +1,19 @@
+# Testing to see if I have the correct imports
+# test_libs.py
+# By Matthew Oakley
+# September 9th 2017
+import sys
+import numpy
+import scipy
+import sklearn
+import matplotlib
+import pandas
+import tensorflow as tf
+
+print(sys.version)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(matplotlib.__version__)
+print(pandas.__version__)
+print(tf.__version__)
