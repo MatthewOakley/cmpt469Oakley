5e3a96e5f3bb6cf1ab3d98ae7cd9629ba40649f1
unknown <matthew.oakley1@marist.edu>
2017-09-25 12:03:17 -0400

    Added the lab report for lab 1

--- labs/hw0/question3.py
+++ labs/hw0/question3.py
@@ -1,3 +1,9 @@
+# question3.py
+# Written by Matthew Oakley
+# 9/11/17
+# This program gets the rank to two matrices
+# that are multiplied together
+
 import numpy as np
 
 a = np.matrix('1 4 -3; 2 -1 3')
@@ -18,3 +24,5 @@ print(c)
 print("Matrix A * Matrix B:")
 print(c.dot(b))
 
+# gives weird error but still works
+print(np.rank((c.dot(b))))
